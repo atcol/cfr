145868775d4e78bf9e5dfdf357ad94ec3e0398d2
Alex Collins <alextcollins@gmail.com>
2013-03-10 14:32:19 +0000

    Document types in cpool_t

--- src/class.h
+++ src/class.h
@@ -7,17 +7,17 @@
 #include <stdlib.h>
 
 enum cpool_t {
-	STRING_UTF8 = 1, /* occupies 2+x bytes */
-	INTEGER = 3,
-	FLOAT = 4,
-	LONG = 5,
-	DOUBLE = 6,
-	CLASS = 7,
-	STRING = 8,
-	FIELD = 9,
-	METHOD = 10,
-	INTERFACE_METHOD = 11,
-	NAME = 12
+	STRING_UTF8	= 1, /* occupies 2+x bytes */
+	INTEGER 	= 3, /* 32bit two's-compliment big endian int */
+	FLOAT 		= 4, /* 32-bit single precision */
+	LONG 		= 5, /* Long: a signed 64-bit two's complement number in big-endian format (takes two slots in the constant pool table) */
+	DOUBLE 		= 6, /* Double: a 64-bit double-precision IEEE 754 floating-point number (takes two slots in the constant pool table) */
+	CLASS 		= 7, /* Class reference: an index within the constant pool to a UTF-8 string containing the fully qualified class name (in internal format) */
+	STRING 		= 8, /* String reference: an index within the constant pool to a UTF-8 string */
+	FIELD 		= 9, /* Field reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
+	METHOD 		= 10, /* Method reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
+	INTERFACE_METHOD = 11, /* Interface method reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
+	NAME 		= 12 /* Name and type descriptor: 2 indexes to UTF-8 strings, the first representing a name and the second a specially encoded type descriptor. */
 };
 
 /*
