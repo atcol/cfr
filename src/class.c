994a225ff60b6a0e44a02b43de5ac8db7525ad05
atc <atc@alexcollins.org>
2013-03-19 12:03:07 +0000

    Isolate vars in case 1 for string read; add docs

--- src/class.c
+++ src/class.c
@@ -36,72 +36,74 @@ Class read_class(const ClassFile class_file) {
 		printf("Tag byte: %d\n", tag_byte);
 		switch (tag_byte) {
 			case 1: 
-					fread(&index, sizeof(index), 1, class_file.file);
-					table_size_bytes += 2 + index;
-					printf("String size (bytes): %d\n", index);
-					str = malloc(sizeof(char) * index);
-					fread(str, sizeof(char), index, class_file.file);
-					printf("String: %s\n", str);
-					free(str);
-					break;
-			case 3:
-					fread(&int32, sizeof(int32), 1, class_file.file);
-					printf("int32: %d\n", int32);
-					table_size_bytes += 4;
-					break;
-			case 4:
-					fread(&flt, sizeof(flt), 1, class_file.file);
-					printf("float: %f\n", flt);
-					table_size_bytes += 4;
-					break;
-			case 5:
-					fread(&lng, sizeof(lng), 1, class_file.file);
-					printf("long: %ld\n", lng);
-					table_size_bytes += 8;
-					break;
-			case 6:
-					fread(&dbl, sizeof(dbl), 1, class_file.file);
-					printf("double: %f\n", dbl);
-					table_size_bytes += 8;
-					break;
-			case 7:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Class index: %d\n", index);
-					table_size_bytes += 2;
-					break;
-			case 8:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("String index: %d\n", index);
-					table_size_bytes += 2;
-					break;
-			case 9:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Field index 1: %d\n", index);
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Field index 1: %d\n", index);
-					table_size_bytes += 4;
-					break;
-			case 10:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Method index 1: %d\n", index);
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Method index 2: %d\n", index);
-					table_size_bytes += 4;
-					break;
-			case 11:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Interface index 1: %d\n", index);
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Interface index 2: %d\n", index);
-					table_size_bytes += 4;
-					break;
-			case 12:
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Name index 1: %d\n", index);
-					fread(&index, sizeof(index), 1, class_file.file);
-					printf("Name index 2: %d\n", index);
-					table_size_bytes += 4;
-					break;
+				/* 1st 2 bytes are int denoting number of bytes in string that follows */
+				uint16_t str_size; 
+				fread(&str_size, sizeof(str_size), 1, class_file.file);
+				printf("String size (bytes): %d\n", str_size);
+				str = malloc(sizeof(char) * str_size);
+				fread(str, sizeof(char), str_size, class_file.file);
+				printf("String: %s\n", str);
+				free(str);
+				table_size_bytes += 2 + str_size;
+				break;
+			case 3: // Integer: a signed 32-bit two's complement number in big-endian format
+				fread(&int32, sizeof(int32), 1, class_file.file);
+				printf("int32: %d\n", int32);
+				table_size_bytes += 4;
+				break;
+			case 4: // Float: a 32-bit single-precision IEEE 754 floating-point number
+				fread(&flt, sizeof(flt), 1, class_file.file);
+				printf("float: %f\n", flt);
+				table_size_bytes += 4;
+				break;
+			case 5: // Long: a signed 64-bit two's complement number in big-endian format (takes two slots in the constant pool table)
+				fread(&lng, sizeof(lng), 1, class_file.file);
+				printf("long: %ld\n", lng);
+				table_size_bytes += 8;
+				break;
+			case 6: // Double: a 64-bit double-precision IEEE 754 floating-point number (takes two slots in the constant pool table)
+				fread(&dbl, sizeof(dbl), 1, class_file.file);
+				printf("double: %f\n", dbl);
+				table_size_bytes += 8;
+				break;
+			case 7: // Class reference: an index within the constant pool to a UTF-8 string containing the fully qualified class name
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Class index: %d\n", index);
+				table_size_bytes += 2;
+				break;
+			case 8: // String reference: an index within the constant pool to a UTF-8 string
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("String index: %d\n", index);
+				table_size_bytes += 2;
+				break;
+			case 9: // Field reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Field index 1: %d\n", index);
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Field index 1: %d\n", index);
+				table_size_bytes += 4;
+				break;
+			case 10: // Method reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Method index 1: %d\n", index);
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Method index 2: %d\n", index);
+				table_size_bytes += 4;
+				break;
+			case 11: // Interface method reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Interface index 1: %d\n", index);
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Interface index 2: %d\n", index);
+				table_size_bytes += 4;
+				break;
+			case 12: // Name and type descriptor: two indexes to UTF-8 strings, 1st representing a name (identifier), 2nd a specially encoded type descriptor
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Name index 1: %d\n", index);
+				fread(&index, sizeof(index), 1, class_file.file);
+				printf("Name index 2: %d\n", index);
+				table_size_bytes += 4;
+				break;
 		}
 		fread(&tag_byte, sizeof(char), 1, class_file.file);
 	}
