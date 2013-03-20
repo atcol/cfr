ac09e613f7b933e6dfb3d869749d739d8664dc07
atc <atc@alexcollins.org>
2013-03-20 10:40:48 +0000

    Observe big endian for indexes; parse utf-8 string

--- src/class.c
+++ src/class.c
@@ -28,27 +28,35 @@ Class read_class(const ClassFile class_file) {
 	float flt;
 	long lng;
 	double dbl;
-	uint16_t index;
+	uint16_t uint16;
 	char *str;
+	UT_hash_table pool = {}
 	
-	fread(&tag_byte, sizeof(char), 1, class_file.file);
 	for (i = 0; i < const_pool_size; i++) {
+		fread(&tag_byte, sizeof(char), 1, class_file.file);
 		printf("Tag byte: %d\n", tag_byte);
 		switch (tag_byte) {
-			case 1: 
-				/* 1st 2 bytes are int denoting number of bytes in string that follows */
-				uint16_t str_size; 
-				fread(&str_size, sizeof(str_size), 1, class_file.file);
-				printf("String size (bytes): %d\n", str_size);
-				str = malloc(sizeof(char) * str_size);
-				fread(str, sizeof(char), str_size, class_file.file);
+			case 1: // String prefixed by a 16-bit number (type u2) indicating the number of bytes in the encoded string which immediately follows
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				uint16 = be16toh(uint16);
+				printf("String size (bytes): %d\n", uint16);
+				str = malloc(sizeof(char) * uint16);
+				char x, y;
+				int i, j;
+				for (i = 0, j = 0; i < uint16; i += 2, j++) { // read 2 at a time
+					fread(&x, sizeof(char), 1, class_file.file);
+					fread(&y, sizeof(char), 1, class_file.file);
+					str[j] = x;
+					j++;
+					str[j] = y;
+				}
 				printf("String: %s\n", str);
 				free(str);
-				table_size_bytes += 2 + str_size;
+				table_size_bytes += 2 + uint16;
 				break;
 			case 3: // Integer: a signed 32-bit two's complement number in big-endian format
 				fread(&int32, sizeof(int32), 1, class_file.file);
-				printf("int32: %d\n", int32);
+				printf("int32: %d\n", be32toh(int32));
 				table_size_bytes += 4;
 				break;
 			case 4: // Float: a 32-bit single-precision IEEE 754 floating-point number
@@ -58,7 +66,7 @@ Class read_class(const ClassFile class_file) {
 				break;
 			case 5: // Long: a signed 64-bit two's complement number in big-endian format (takes two slots in the constant pool table)
 				fread(&lng, sizeof(lng), 1, class_file.file);
-				printf("long: %ld\n", lng);
+				printf("long: %ld\n", be64toh(lng));
 				table_size_bytes += 8;
 				break;
 			case 6: // Double: a 64-bit double-precision IEEE 754 floating-point number (takes two slots in the constant pool table)
@@ -66,46 +74,45 @@ Class read_class(const ClassFile class_file) {
 				printf("double: %f\n", dbl);
 				table_size_bytes += 8;
 				break;
-			case 7: // Class reference: an index within the constant pool to a UTF-8 string containing the fully qualified class name
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Class index: %d\n", index);
+			case 7: // Class reference: an uint16 within the constant pool to a UTF-8 string containing the fully qualified class name
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Class uint16: %d\n", be16toh(uint16));
 				table_size_bytes += 2;
 				break;
-			case 8: // String reference: an index within the constant pool to a UTF-8 string
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("String index: %d\n", index);
+			case 8: // String reference: an uint16 within the constant pool to a UTF-8 string
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("String uint16: %d\n", be16toh(uint16));
 				table_size_bytes += 2;
 				break;
-			case 9: // Field reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Field index 1: %d\n", index);
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Field index 1: %d\n", index);
+			case 9: // Field reference: two uint16es within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Field uint16 1: %d\n", be16toh(uint16));
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Field uint16 2: %d\n", be16toh(uint16));
 				table_size_bytes += 4;
 				break;
-			case 10: // Method reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Method index 1: %d\n", index);
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Method index 2: %d\n", index);
+			case 10: // Method reference: two uint16s within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Method uint16 1: %d\n", be16toh(uint16));
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Method uint16 2: %d\n", be16toh(uint16));
 				table_size_bytes += 4;
 				break;
-			case 11: // Interface method reference: two indexes within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Interface index 1: %d\n", index);
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Interface index 2: %d\n", index);
+			case 11: // Interface method reference: 2 uint16 within the pool, 1st pointing to a Class reference, 2nd to a Name and Type descriptor
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Interface uint16 1: %d\n", be16toh(uint16));
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Interface uint16 2: %d\n", be16toh(uint16));
 				table_size_bytes += 4;
 				break;
-			case 12: // Name and type descriptor: two indexes to UTF-8 strings, 1st representing a name (identifier), 2nd a specially encoded type descriptor
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Name index 1: %d\n", index);
-				fread(&index, sizeof(index), 1, class_file.file);
-				printf("Name index 2: %d\n", index);
+			case 12: // Name and type descriptor: 2 uint16 to UTF-8 strings, 1st representing a name (identifier), 2nd a specially encoded type descriptor
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Name uint16 1: %d\n", be16toh(uint16));
+				fread(&uint16, sizeof(uint16), 1, class_file.file);
+				printf("Name uint16 2: %d\n", be16toh(uint16));
 				table_size_bytes += 4;
 				break;
 		}
-		fread(&tag_byte, sizeof(char), 1, class_file.file);
 	}
 
 	printf("Table size: %d\n", table_size_bytes);
@@ -115,7 +122,8 @@ Class read_class(const ClassFile class_file) {
 		minor_version,
 		major_version,
 		const_pool_size,
-		table_size_bytes
+		table_size_bytes,
+		pool
 	};
 
 	return class;
@@ -132,5 +140,5 @@ void print_class(FILE *stream, const Class class) {
 	fprintf(stream, "Minor number: %u \n", class.minor_version);
 	fprintf(stream, "Major number: %u \n", class.major_version);
 	fprintf(stream, "Constant pool size: %u \n", class.const_pool_size);
-	fprintf(stream, "Constant table size: %ub \n", class.const_table_size_bytes);
+	fprintf(stream, "Constant table size: %ub \n", class.pool_size_bytes);
 }
