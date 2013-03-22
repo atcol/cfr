84a22e941ff9d953ad1d4f8b824f15f181d971b8
atc <atc@alexcollins.org>
2013-03-22 12:57:33 +0000

    Add MethodHandle,MethodType and InvokeDynamic

--- src/class.h
+++ src/class.h
@@ -8,17 +8,20 @@
 #include "uthash.h"
 
 enum cpool_t {
-	STRING_UTF8     = 1, /* occupies 2+x bytes */
-	INTEGER         = 3, /* 32bit two's-compliment big endian int */
-	FLOAT           = 4, /* 32-bit single precision */
-	LONG            = 5, /* Long: a signed 64-bit two's complement number in big-endian format (takes two slots in the constant pool table) */
-	DOUBLE          = 6, /* Double: a 64-bit double-precision IEEE 754 floating-point number (takes two slots in the constant pool table) */
-	CLASS           = 7, /* Class reference: an index within the constant pool to a UTF-8 string containing the fully qualified class name (in internal format) */
-	STRING          = 8, /* String reference: an index within the constant pool to a UTF-8 string */
-	FIELD           = 9, /* Field reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
-	METHOD          = 10, /* Method reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
+	STRING_UTF8      = 1, /* occupies 2+x bytes */
+	INTEGER          = 3, /* 32bit two's-compliment big endian int */
+	FLOAT            = 4, /* 32-bit single precision */
+	LONG             = 5, /* Long: a signed 64-bit two's complement number in big-endian format (takes two slots in the constant pool table) */
+	DOUBLE           = 6, /* Double: a 64-bit double-precision IEEE 754 floating-point number (takes two slots in the constant pool table) */
+	CLASS            = 7, /* Class reference: an index within the constant pool to a UTF-8 string containing the fully qualified class name (in internal format) */
+	STRING           = 8, /* String reference: an index within the constant pool to a UTF-8 string */
+	FIELD            = 9, /* Field reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
+	METHOD           = 10, /* Method reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
 	INTERFACE_METHOD = 11, /* Interface method reference: two indexes within the constant pool, the first pointing to a Class reference, the second to a Name and Type descriptor. */
-	NAME            = 12 /* Name and type descriptor: 2 indexes to UTF-8 strings, the first representing a name and the second a specially encoded type descriptor. */
+	NAME             = 12, /* Name and type descriptor: 2 indexes to UTF-8 strings, the first representing a name and the second a specially encoded type descriptor. */
+	METHOD_HANDLE 	 = 15,
+	METHOD_TYPE 	 = 16,
+	INVOKE_DYNAMIC 	 = 18
 };
 
 /* A wrapper for FILE structs that also holds the file name.  */
