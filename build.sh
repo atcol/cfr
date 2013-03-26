5287ae73d11d7a866071cab962a992e9b5638bc7
atc <atc@alexcollins.org>
2013-03-26 16:39:56 +0000

    Add build.sh for just building

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clear; scons -c; scons 
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-clear; scons -c; scons && clear && ./cfr test/files/Test.class test/files/RabbitQueueClient.class test/files/RabbitQueueConsumer.class /dev/urandom test/files/LongTest.class test/files/DoubleTest.class test/files/Interfaces.class test/files/InvalidTagByte.class
+./build.sh && ./cfr test/files/Test.class test/files/RabbitQueueClient.class test/files/RabbitQueueConsumer.class /dev/urandom test/files/LongTest.class test/files/DoubleTest.class test/files/Interfaces.class test/files/InvalidTagByte.class
