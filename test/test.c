39bd24f1e845775fc7cb1f5a989ecb3b2a0db689
Alex Collins <alextcollins@gmail.com>
2013-04-06 15:25:24 +0100

    Add basic test

--- test/test.c
+++ test/test.c
@@ -1,12 +1,27 @@
-#include <stdio.h>
-#include "tap.h"
 #include "../src/class.h"
+#include "../src/class.c"
+#include "tap.h"
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
 	plan_tests(1);
 
-	ClassFile *c = read_class_from_file_name("files/DoubleTest.class");
+	char *double_file = "files/DoubleTest.class";
+	Class *c = read_class_from_file_name(double_file);
 
-	ok(1 == 1, "testing");
+	ok((strcmp(c->file_name, double_file) == 0), "testing", NULL);
+	ok(0 == c->minor_version, "Major version is 0 (1.7.0_10)", NULL);
+	ok(51 == c->major_version, "Major version is 51 (1.7)", NULL);
+	// Always assert filename;
+	// 	const pool size 1 greater than number in pool
+	//
+	//
+	
+	free((void *) c);
+
+	c = read_class_from_file_name("files/Fields.class");
+	ok(c->fields_count == 7, "Fields count == 7", NULL);
+
 	return exit_status();
 }
