f582149f1c61f02fc03a9f96aec255295da2cb36
Alex Collins <alextcollins@gmail.com>
2013-05-05 16:32:02 +0100

    Parse attr->info correctly; remote print_class

--- src/class.c
+++ src/class.c
@@ -83,13 +83,11 @@ Class *read_class(const ClassFile class_file) {
 		while (aidx++ < f->attrs_count) {
 			attr = f->attrs + aidx;
 			fread(&attr->name_idx, sizeof(u2), 1, class_file.file);
+			fread(&attr->length, sizeof(u4), 1, class_file.file);
 			attr->name_idx = be16toh(attr->name_idx);
-			printf("name idx is %u\n", attr->name_idx);
-			fread(&attr->length, sizeof(char), 4, class_file.file);
-			//fread(&attr->length, sizeof(u4), 1, class_file.file);
-			printf("len is %d", attr->length);
-			fread(&attr->info, sizeof(char), attr->length, class_file.file);
-			printf("Read attr: %u,%d,%s\n", attr->name_idx, attr->length, attr->info);
+			attr->length = be32toh(attr->length);
+			attr->info = calloc(attr->length, sizeof(char));
+			fread(attr->info, sizeof(char), attr->length, class_file.file);
 		}
 		idx++;
 	}
@@ -292,101 +290,3 @@ char *field2str(const char fld_type) {
 	}
 }
 
-void print_class(FILE *stream, const Class *class) {
-	fprintf(stream, "File: %s\n", class->file_name);
-	fprintf(stream, "Minor number: %u \n", class->minor_version);
-	fprintf(stream, "Major number: %u \n", class->major_version);
-	fprintf(stream, "Constant pool size: %u \n", class->const_pool_count);
-	fprintf(stream, "Constant table size: %ub \n", class->pool_size_bytes);
-	fprintf(stream, "Printing constant pool of %d items...\n", class->const_pool_count-1);
-
-	Item *s;
-	uint16_t i = 1; // constant pool indexes start at 1, get_item converts to pointer index
-	while (i < class->const_pool_count) {
-		s = get_item(class, i);
-		fprintf(stream, "Item #%u %s: ", i, tag2str(s->tag));
-		if (s->tag == STRING_UTF8) {
-			fprintf(stream, "%s\n", s->value.string.value);
-		} else if (s->tag == INTEGER) {
-			fprintf(stream, "%d\n", s->value.integer);
-		} else if (s->tag == FLOAT) {
-			fprintf(stream, "%f\n", s->value.flt);
-		} else if (s->tag == LONG) {
-			fprintf(stream, "%ld\n", to_long(s->value.lng));
-		} else if (s->tag == DOUBLE) {
-			fprintf(stream, "%lf\n", to_double(s->value.dbl));
-		} else if (s->tag == CLASS || s->tag == STRING) {
-			fprintf(stream, "%u\n", s->value.ref.class_idx);
-		} else if(s->tag == FIELD || s->tag == METHOD || s->tag == INTERFACE_METHOD || s->tag == NAME) {
-			fprintf(stream, "%u.%u\n", s->value.ref.class_idx, s->value.ref.name_idx);
-		} //else {
-		//	fprintf(stream, "Don't know how to print item # %d of type %d\n", i, s->tag);
-		//}
-		i++;
-	}
-
-	fprintf(stream, "Access flags: %d\n", class->flags); //TODO use bitwise ops to for printing flags e.g. switch
-
-	Item *cl_str = get_class_string(class, class->this_class);
-	fprintf(stream, "This class: %s\n", cl_str->value.string.value);
-
-	cl_str = get_class_string(class, class->super_class);
-	fprintf(stream, "Super class: %s\n", cl_str->value.string.value);
-
-	fprintf(stream, "Interfaces count: %u\n", class->interfaces_count);
-
-	fprintf(stream, "Printing %u interfaces...\n", class->interfaces_count);
-	if (class->interfaces_count > 0) {
-		Ref *iface = class->interfaces;
-		Item *the_class;
-		uint16_t idx = 0;
-		while (idx < class->interfaces_count) {
-			the_class = get_item(class, iface->class_idx); // the interface class reference
-			Item *item = get_item(class, the_class->value.ref.class_idx);
-			String string = item->value.string;
-			fprintf(stream, "Interface: %s\n", string.value);
-			idx++;
-			iface = class->interfaces + idx; // next Ref
-		}
-	}
-
-	fprintf(stream, "Printing %d fields...\n", class->fields_count);
-
-	if (class->fields_count > 0) {
-		Field *field = class->fields;
-		uint16_t idx = 0;
-		while (idx < class->fields_count) {
-			Item *name = get_item(class, field->name_idx);
-			Item *desc = get_item(class, field->desc_idx);
-			printf("%s %s\n", field2str(desc->value.string.value[0]), name->value.string.value);
-			Attribute at;
-			if (field->attrs_count == 0) {
-				fprintf(stream, "No attributes to print\n");
-			} else {
-				int aidx = 0;
-				while (aidx < field->attrs_count) {
-					at = field->attrs[aidx];
-					fprintf(stream, "\tAttribute length %d", at.length);
-					fprintf(stream, "\tAttribute: %s", at.info);
-					aidx++;
-				}
-			}
-			idx++;
-			field = class->fields + idx;
-		}
-	}
-
-	fprintf(stream, "Printing %u methods...\n", class->methods_count);
-	i = 0;
-	if (class->methods_count > 0) {
-		Method *method = class->methods;
-		uint16_t idx = 0;
-		while (idx < class->methods_count) {
-			Item *name = get_item(class, method->name_idx);
-			//Item *desc = get_item(class, method->desc_idx);
-			printf("%s\n", name->value.string.value);
-			idx++;
-			method = class->methods + idx;
-		}
-	}
-}
