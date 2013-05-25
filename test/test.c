13f22b7a07354dc064a12d943cff9332e6f7df1f
Alex <alextcollins@gmail.com>
2013-05-25 14:47:55 +0100

    DoubleTest constant pool assertions

--- test/test.c
+++ test/test.c
@@ -10,6 +10,18 @@ int main(void) {
 	empty();
 	test_field2str();
 	return exit_status();
+}	
+
+/* Print i and j before calling ok(i == j, msg); */
+void iok(int i, int j, char *msg) {
+	printf("%d == %d \n", i, j, msg);
+	ok(i == j, msg);
+}
+
+/* Print str1 and str2 before calling ok(0 == strcmp(str1, str2), msg); */
+void strok(char *str1, char *str2, char *msg) {
+	printf("'%s' == '%s' \n", str1, str2);
+	ok(0 == strcmp(str1, str2), msg);
 }
 
 void dbl() {
@@ -34,6 +46,54 @@ void dbl() {
 	const Item *attr_name = get_item(c, c->fields[0].attrs[0].name_idx);
 	ok(strcmp("ConstantValue", attr_name->value.string.value) == 0, "First attribute in first field has name ConstantValue");
 
+	// Constant pool content tests
+	Item *i = get_item(c, 1);
+	ok(17 == i->value.ref.name_idx, " 1 = Methodref			   6");
+	ok(6 == i->value.ref.class_idx, " 1 = Methodref          17         //  java/lang/Object.\"<init>\":()V");
+
+	i = get_item(c, 2);
+	ok(18 == i->value.ref.class_idx, " 2 = Fieldref           18        //  java/lang/System.out:Ljava/io/PrintStream;");
+	ok(19 == i->value.ref.name_idx, " 2 = Fieldref           19        //  java/lang/System.out:Ljava/io/PrintStream;");
+
+	i = get_item(c, 3);
+	ok(20 == i->value.ref.class_idx, " 3 = String             20            //  Hello world1.0");
+
+	i = get_item(c, 4);
+	ok(21 == i->value.ref.class_idx, " 4 = Methodref         21        //  java/io/PrintStream.println:(Ljava/lang/String;)V");
+	ok(22 == i->value.ref.name_idx, " 4 = Methodref          22        //  java/io/PrintStream.println:(Ljava/lang/String;)V");
+
+	i = get_item(c, 5);
+	iok(23, i->value.ref.class_idx, " 5 = Class              23            //  DoubleTest");
+
+	i = get_item(c, 6);
+	iok(24, i->value.ref.class_idx, " 6 = Class              24            //  java/lang/Object");
+
+	i = get_item(c, 7);
+	ok(0 == strcmp("d", i->value.string.value), " 7 = Utf8               d");
+
+	//ok(, " 8 = Utf8               D");
+	//ok(, " 9 = Utf8               ConstantValue");
+	//ok(, " 10 = Double             1.0d");
+	//ok(, " 12 = Utf8               <init>");
+	//ok(, " 13 = Utf8               ()V");
+	//ok(, " 14 = Utf8               Code");
+	//ok(, " 15 = Utf8               main");
+	//ok(, " 16 = Utf8               ([Ljava/lang/String;)V");
+	//ok(, " 17 = NameAndType        12:13        //  \"<init>\":()V");
+	//ok(, " 18 = Class              25            //  java/lang/System");
+	//ok(, " 19 = NameAndType        26:27        //  out:Ljava/io/PrintStream;");
+	//ok(, " 20 = Utf8               Hello world1.0");
+	//ok(, " 21 = Class              28            //  java/io/PrintStream");
+	//ok(, " 22 = NameAndType        29:30        //  println:(Ljava/lang/String;)V");
+	//ok(, " 23 = Utf8               DoubleTest");
+	//ok(, " 24 = Utf8               java/lang/Object");
+	//ok(, " 25 = Utf8               java/lang/System");
+	//ok(, " 26 = Utf8               out");
+	//ok(, " 27 = Utf8               Ljava/io/PrintStream;");
+	//ok(, " 28 = Utf8               java/io/PrintStream");
+	//ok(, " 29 = Utf8               println");
+	//ok(, " 30 = Utf8               (Ljava/lang/String;)V");
+
 	const Method *method = c->methods;
 	const Item *m1name = get_item(c, c->methods[0].name_idx);
 	const Item *m2name = get_item(c, c->methods[1].name_idx);
@@ -42,7 +102,7 @@ void dbl() {
 	ok(0 == strcmp("main", m2name->value.string.value), "Second method's name is main");
 	ok(1 == method->attrs_count, "init method attribute count is 1");
 
-	// Test that attribute #1 (index 0) has the name "Code"
+	// Test that attribute 1 (index 0) has the name "Code"
 	const Item *m1attr1 = get_item(c, method->attrs[0].name_idx);
 	ok(0 == strcmp("Code", m1attr1->value.string.value), "Attribute #1 of method #1 has name Code");
 	ok(1 == c->methods[1].attrs_count, "main method attribute count is 1");
